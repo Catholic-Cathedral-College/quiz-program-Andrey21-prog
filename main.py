1fe58cd7a78582e9a3c9da4e7991a49d4fe93802
AndreiB7 <112134893+Andrey21-prog@users.noreply.github.com>
2022-09-08 23:43:47 +0000

    added more fun questions

--- main.py
+++ main.py
@@ -3,24 +3,17 @@ restart = 1
 
 while restart == 1:
   
-  
-  
   i = 0
+  print('WELCOME TO THE NOT GREATEST QUIZ GAME BY ME')
   
-  print('WELCOME TO MY QUIZ ')
-  
-  
-  user_name = input('What is your Name?')
-  
+  user_name = input('What is your User Name?')
   
   print ('\nHI THERE ' + user_name + '! LET\'S PLAY A GAME or you could say a QUIZ!\n')
   print ('So you basically you have a question in front of you.\n\nYou have options and you write if it is (a,b,c, or d) not (A, B, C, or D)')
   print ('and if you get it correct you get a point.\n\n if you get it incorrect you lose a point and remember this is case sensitive.\n')
   print ('Also if you start from level 1 you have a chance of attempting the BONUS QUESTION!\n')
   
-  
   score = 0
-  
   
   while i<1:
     ready = input("Are You Ready? Type 'yes' or 'no'\n")
@@ -31,7 +24,7 @@ while restart == 1:
   
   user_level_choice = input('What level do you like to start from, Easy or Hard. Type level 1 or level 2:\n')
   if user_level_choice == 'level 1': 
-    
+     
     print("Question 1: The number of players in a court from 1 team? \n")
     print("a: Players 12 ")
     print("b: Players 9  ")
@@ -125,4 +118,215 @@ while restart == 1:
     if level_2 == "continue" or level_2 == "yes":
       print ('have 2 free points for keeping up\n!')
       score += 2
-      print ('your score is {}\n\n'.format(score))
+      print ('your score is {}\n\n'.format(score))  
+
+  
+      print("Question 6: When was basketball made?\n")
+      print("a:1737 ")
+      print("b: 1891 ")
+  
+      response = input("What is your answer to this question?\n")
+      if response == "b":
+        print("Correct, way to go")
+        score += 2
+      else:
+        print("sorry, not bad")
+      score -= 1
+      print("your score is {}\n".format(score))
+  
+  
+  
+      print("Question 7: 4. Who made basketball?\n")
+      print("a: James Naismith ")
+      print("b: William Morgan")
+      print("c: Jim Thorpe")
+  
+      response = input("What is your answer to this question?\n")
+      if response == "a":
+        print("Correct, nice job here have a point")
+        score += 2
+      else:
+        print("sorry, next time")
+      score -= 1
+      print("your score is {}\n".format(score))
+  
+  
+      print("Question 8: What is a turnover?\n")
+      print("a:When the two teams switch.")
+      print("b:  The different ways that possession of the ball goes from one team to another.")
+      print("c:A player is transferred to the other team during a game.")
+    
+      
+      response = input("What is your answer to this question?") 
+      if response == "b":
+        print("Correct, don't stop!")
+        score += 2
+      else:
+        print("sorry, but next time")
+      score -= 1
+      print("your score is {}\n".format(score))
+  
+      print("Question 9: What does BEEF mean?\n")
+      print("a: Balance, Eyes, Elbow, Follow Through")
+      print("b: Basketball, Elbow, Endline, Free-Throw")
+      print("c: Box and One, Euro Step, Elevator Screen, Field Goal")
+      
+  
+      response = input("What is your answer to this question?\n")
+      if response == "a" or response == "b":
+        print("Correct, nice job here have a point")
+        score += 2
+      else:
+        print("sorry, next time")
+      score -= 1
+      print("your score is {}\n".format(score))
+  
+  
+      print("Question 10: The main responsibility of the “center” player is to…\n")
+      print("a: make baskets ")
+      print("b: steal the ball and block attacks")
+      print("c: Tennis")
+      print("d:  secure rebounds and defend the paint")
+  
+      response = input("What is your answer to this question?\n")
+      if response == "c":
+        print("Correct, nice job here have a point\n\n")
+        score += 2
+      else:
+        print("sorry, next time\n")
+      score -= 1
+      print('your score is {}'.format(score))
+      
+      if score>8:
+        print('Wow amazing performance try this bonus question!\n\n')
+  
+      if score>8 :
+        print('Bonus Question: What is the abbreviation of our school?')
+        print('a: Papa High')
+        print("b: Papatoetoe High School")
+        print("c: PHS")
+        print("d: None of the above\n")
+  
+      if score>8 :
+        response = input("What is your answer to this question?\n")
+      if response == "c":
+        print("Out-standing performance keep it up!\n")
+        score += 2
+      else:
+        print("No worries no one's perfect but we can be perfect\n")
+      score -= 1
+      print("your Final score is {}!\n\n".format(score))
+        
+      print("THANKS FOR PLAYING!")
+  
+      PLAYERS_FEEDBACK = input(" Please rate this quiz out of ten and tell us what we could improve on \n")
+  
+      play_again = input("Do you want to restart? Yes or No\n")
+  
+      if play_again == "Yes" or play_again == "yes":
+          exec(open("./Python quiz by Tanish.py").read())
+      else:
+          exit()
+  
+  
+      print("Great choice\n\n")
+    else:
+      quit()
+  
+      
+  else:
+    print("Question 6: It takes 16 seconds for the chug jug in Fortnite Battle royale?\n")
+    print("a: True ")
+    print("b: False ")
+  
+    response = input("What is your answer to this question?\n")
+    if response == "b":
+      print("Correct, way to go")
+      score += 2
+    else:
+      print("sorry, not bad")
+    score -= 1
+    print("your score is {}\n".format(score))
+  
+  
+  
+    print("Question 7: Which planet did Superman come from?\n")
+    print("a: Krypton ")
+    print("b: Kripton")
+    print("c: crypton")
+  
+    response = input("What is your answer to this question?\n")
+    if response == "a":
+      print("Correct, nice job here have a point")
+      score += 2
+    else:
+      print("sorry, next time")
+    score -= 1
+    print("your score is {}\n".format(score))
+  
+  
+    print("Question 8: In a standard pack of cards, which king is the only one to not have a moustache?\n")
+    print("a: king of heart")
+    print("b: knig of spades")
+    print("c: king of clubs")
+    print("d: queen of heart")
+      
+    response = input("What is your answer to this question?\n") 
+    if response == "a":
+      print("Correct, don't stop!")
+      score += 2
+    else:
+      print("sorry, but next time")
+      score -= 1
+    print("your score is {}\n".format(score))
+  
+  
+    print("Question 9: What is the standard unit of distance in the metric system?\n")
+    print("a: meter ")
+    print("b: kilometer")
+    print("c: miles")
+    print("d: grams")
+  
+    response = input("What is your answer to this question?\n")
+    if response == "a" or response == "b":
+      print("Correct, nice job here have a point")
+      score += 2
+    else:
+      print("sorry, next time")
+    score -= 1
+    print("your score is {}\n".format(score))
+  
+  
+    print("Question 10: What game features the terms love, deuce, match and volley?\n")
+    print("a: Soccer ")
+    print("b: basketball")
+    print("c: Tennis")
+    print("d: badminton-maybe this one")
+  
+    response = input("What is your answer to this question?\n")
+    if response == "c":
+      print("Correct, nice job here have a point")
+      score += 2
+    else:
+      print("sorry, next time\n")
+    score -= 1
+  
+    print("your Final score is {}!\n\n".format(score))
+      
+    print("THANKS FOR PLAYING!")
+  
+    PLAYERS_FEEDBACK = input(" Please rate this quiz out of ten and tell us what we could improve on \n")
+  
+    play_again = input("Do you want to restart/play again? Yes or No\n")
+  
+    if play_again == "Yes" or play_again == "yes":
+        restart = 1 
+    else:
+        restart = 0
+        exit()
+  
+
+
+
+
+
